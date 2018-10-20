24b0568e3b948407dbe5147536a9b3a450e87337
Shaqeeb Momen <shaqeebmomen@users.noreply.github.com>
2018-10-20 16:39:19 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,68 @@
+let Keyboard = window.SimpleKeyboard.default;
+
+let keyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button),
+  theme: "hg-theme-default hg-layout-default myTheme"
+
+
+});
+
+console.log(keyboard);
+
+// document.querySelector(".input").addEventListener("change", event => {
+//   keyboard.setInput(event.target.value);
+// });
+
+function onChange(input) {
+  document.querySelector(".input").value = input;
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+  console.log("Button pressed", button);
+
+  /**
+   * If you want to handle the shift and caps lock buttons
+   */
+  if (button === "{shift}" || button === "{lock}") {
+    handleShift();
+  }
+  if (button === "{enter}") {
+    console.log("THE ENTER KEY WAS PRESSED");
+    //Put the text to speech stuff here
+    }
+  }
+
+
+function handleShift() {
+  console.log("Shifting");
+  let currentLayout = keyboard.options.layoutName;
+  if (currentLayout === 'default') {
+    currentLayout = 'shift';
+  }
+  else if (currentLayout = 'shift') {
+    currentLayout = 'default'
+  }
+  // let shiftToggle = currentLayout === 'default' ? 'shift' : 'default';
+
+  keyboard.setOptions({
+    layoutName: currentLayout,
+    layout: {
+      'default': [
+      '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+      '{clear} q w e r t y u i o p [ ] \\',
+      '{lock} a s d f g h j k l ; \' {enter}',
+      '{shift} z x c v b n m , . / {shift}',
+      '.com @ {space}'
+      ],
+      'shift': [
+      '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+      '{tab} Q W E R T Y U I O P { } |',
+      '{lock} A S D F G H J K L : " {enter}',
+      '{shift} Z X C V B N M < > ? {shift}',
+      '.com @ {space}'
+      ]
+    },
+  });
+}
